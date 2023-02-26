31f49092e3b64fd57245aeed1a97c3d689cf8362
anatoliy-chaika <110462151+anatoliy-chaika@users.noreply.github.com>
2023-02-26 14:50:50 +0200

    error

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { ContactItem } from '../ContactItem/ContactItem';
 
 export const Contactlist = ({ data }, onDelete) => {
